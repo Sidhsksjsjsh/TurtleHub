24d8d7deb3d0cf12967d49c699f6f33867f4ef24
Terminated <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-03-02 19:09:15 +0900

    Update Script.lua

--- Zombie-Uprising/Script.lua
+++ Zombie-Uprising/Script.lua
@@ -341,6 +341,73 @@ end)
 end
 })
 
+P3:AddToggle({
+Name = "ESP Body Colors (Zombie only)",
+Default = false,
+Callback = function(v)
+getgenv().enabled = v
+getgenv().filluseteamcolor = false
+getgenv().outlineuseteamcolor = false
+getgenv().fillcolor = Color3.new(172, 0, 0)
+getgenv().outlinecolor = Color3.new(1, 1, 1)
+getgenv().filltrans = 0
+getgenv().outlinetrans = 0
+
+
+local holder = game.CoreGui:FindFirstChild("ESPHolder") or Instance.new("Folder")
+if enabled == false then
+    holder:Destroy()
+end
+
+
+if holder.Name == "Folder" then
+    holder.Name = "ESPHolder"
+    holder.Parent = game.CoreGui
+end
+
+
+if uselocalplayer == false and holder:FindFirstChild(game.Players.LocalPlayer.Name) then
+    holder:FindFirstChild(game.Players.LocalPlayer.Name):Destroy()
+end
+
+
+if getgenv().enabled == true then 
+    getgenv().enabled = false
+    getgenv().enabled = true
+end
+while getgenv().enabled do
+    task.wait()
+    for _,v in pairs(game.Workspace:GetChildren()) do
+        if v.Name == "Zombies" then
+        local chr = v.Character
+        if chr ~= nil then
+        local esp = holder:FindFirstChild(v.Name) or Instance.new("Highlight")
+        esp.Name = v.Name
+        if uselocalplayer == false and esp.Name == game.Workspace:FindFirstChild("Zombies").Name then
+            else
+        esp.Parent = holder
+        if filluseteamcolor then
+            esp.FillColor = v.TeamColor.Color
+        else
+            esp.FillColor = fillcolor 
+        end
+        if outlineuseteamcolor then
+            esp.OutlineColor = v.TeamColor.Color
+        else
+            esp.OutlineColor = outlinecolor    
+        end
+        esp.FillTransparency = filltrans
+        esp.OutlineTransparency = outlinetrans
+        esp.Adornee = chr
+        esp.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+        end
+        end
+    end
+end
+end
+end
+})
+
 P3:AddButton({
 Name = "nearby zombie notifications",
 Callback = function(esp)
